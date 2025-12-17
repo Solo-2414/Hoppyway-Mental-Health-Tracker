387c902135e0d84c224db3e2ef5f2bdb60968204
Solo-2414 <danielrodge.caindoy@gmail.com>
2025-12-18 07:10:07 +0800

    Config update

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
 class Config:
-    # Use the secret key from your code
-    SECRET_KEY = "hoppyway-secret-key-123"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hoppyway-secret-key-123'
     
-    # Database Configuration from your code
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:241405@localhost/hoppy_way_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        "mysql+pymysql://root:241405@localhost/hoppy_way_db"
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Security setting for Bcrypt
+
     BCRYPT_LOG_ROUNDS = 12
