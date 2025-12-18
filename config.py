d78585e6bd3cdb2f6798f9392312b55333f708a2
Solo-2414 <danielrodge.caindoy@gmail.com>
2025-12-18 08:27:22 +0800

    Updated

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hoppyway-secret-key-123'
-    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         "mysql+pymysql://root:241405@localhost/hoppy_way_db"
-        
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    if os.environ.get('DATABASE_URL'):
+        SQLALCHEMY_ENGINE_OPTIONS = {
+            "connect_args": {
+                "ssl": {
+                    "fake_flag_to_trigger_ssl": True
+                }
+            }
+        }
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hoppyway-secret-key-123'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     BCRYPT_LOG_ROUNDS = 12
